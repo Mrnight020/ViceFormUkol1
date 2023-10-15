10038a8e889d90bea1c9e0a00f678c05ee4fd95d
Mrnight020 <97515527+Mrnight020@users.noreply.github.com>
2023-10-15 20:40:52 +0200

    Add files via upload

---
+++ Ukol1/Ukol1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Ukol1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(37, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 127);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(272, 46);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(198, 276);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(476, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 88);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Smazat listbox";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 345);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Ukol1/Ukol1/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Ukol1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Form2 form2;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            form2 = new Form2(listBox1);
+            form2.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+    }
+    public partial class Form2 : Form
+    {
+        Button button2 = new Button();
+        Button button3 = new Button();
+        TextBox textBox1 = new TextBox();
+
+        ListBox listik = new ListBox();
+        public Form2(ListBox listbox)
+        {
+            listik = listbox;
+            this.ClientSize = new System.Drawing.Size(400, 250);
+            // 
+            // textBox1
+            textBox1.Location = new System.Drawing.Point(68, 54);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new System.Drawing.Size(240, 22);
+            textBox1.TabIndex = 3;           
+            textBox1.KeyDown += textBox1_KeyDown;
+            textBox1.Focus();
+            // 
+            // button2
+            button2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            button2.ForeColor = System.Drawing.Color.Black;
+            button2.Location = new System.Drawing.Point(68, 123);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(112, 77);
+            button2.TabIndex = 4;
+            button2.Text = "Ulož";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            button3.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            button3.ForeColor = System.Drawing.Color.Black;
+            button3.Location = new System.Drawing.Point(196, 123);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(112, 77);
+            button3.TabIndex = 5;
+            button3.Text = "Zruš";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (listik.Items.Count == 10)
+            {
+                MessageBox.Show("Maximalně 10 prvku !!!");
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(textBox1.Text))
+                {
+                    MessageBox.Show("Nic si nezadal");
+                }
+                else
+                {
+                    listik.Items.Add("" + textBox1.Text);
+                }
+            }
+            textBox1.Focus();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.Focus();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                this.Close();
+            }
+
+        }
+    }
+
+}
